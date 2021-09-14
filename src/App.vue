81d9f5e81fc22923a5dd17ed44e5f567a09f6f42
Svyatnenko.OV <svyatnenko.ov@dns-shop.ru>
2021-09-14 17:56:07 +1000

    init v2 ;)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <Navbar></Navbar>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+import BoardPage from './components/BoardPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    BoardPage
   }
 }
 </script>
@@ -20,7 +23,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="navbar"></div>
+  <nav> Cruello </nav>
+</template>
+
+<script>
+export default {
+name: "Navbar.vue"
+}
+</script>
+
+<style scoped lang="scss">
+#navbar {
+  width: 100%;
+  display: inline-block;
+}
+
+nav {
+  background-color: #42b983;
+  height: 30px;
+}
+</style>
