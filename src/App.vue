acf7b57c88a57d8dfba9ac5d41f99b72f43ea9f5
Svyatnenko.OV <svyatnenko.ov@dns-shop.ru>
2021-09-17 15:37:23 +1000

    + dropzone div's +titles flex edits working D&D

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,11 @@ export default {
 </script>
 
 <style lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+
 #app {
-  font-family:             Avenir, Helvetica, Arial, sans-serif;
+  font-family:             'Open Sans', sans-serif;
   -webkit-font-smoothing:  antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align:              center;
